af38514dfca8e6e4b269e5eb985a7b106d85bb43
Vitor Pellegrino <vitorp@gmail.com>
2012-02-17 00:36:59 -0200

    Creating card at jackpot before creating the customer locally

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,13 +1,13 @@
 class Customer < ActiveRecord::Base
   attr_writer :credit_card
-  after_create :create_customer_at_jackpot 
+  before_create :create_customer_at_jackpot 
 
   def create_customer_at_jackpot
     # Creates this customer at jackpot
     @jackpot_customer = Jackpot::Customer.create(email: self.email, 
                                                  subscription_id: self.jackpot_subscription_id)
 
-    self.update_attribute(:jackpot_customer_id, @jackpot_customer.id)
+    self.jackpot_customer_id = @jackpot_customer.id 
 
     if @jackpot_customer
       @jackpot_customer.update_credit_card(Jackpot::Card.new(@credit_card))
