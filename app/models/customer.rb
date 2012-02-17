03aa149401d104dd030af69def2d1c25b55a8a04
Vitor Pellegrino <vitorp@gmail.com>
2012-02-17 00:36:19 -0200

    Creating jackpot customer id

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,6 +6,9 @@ class Customer < ActiveRecord::Base
     # Creates this customer at jackpot
     @jackpot_customer = Jackpot::Customer.create(email: self.email, 
                                                  subscription_id: self.jackpot_subscription_id)
+
+    self.update_attribute(:jackpot_customer_id, @jackpot_customer.id)
+
     if @jackpot_customer
       @jackpot_customer.update_credit_card(Jackpot::Card.new(@credit_card))
     end 
